4915980557cce5d6c06e9397b917fa6011f9c7ab
LeungYuHo <97867543+LeungYuHo@users.noreply.github.com>
2023-05-06 02:20:10 +0800

    Add files via upload

---
+++ PromptUsersForInput_final/bin/ConversationFlow.cs
@@ -0,0 +1,29 @@
+﻿namespace Microsoft.BotBuilderSamples
+{
+    public class ConversationFlow
+{
+        // Identifies the last question asked.
+        public enum Question
+        {
+            Name,
+            None, // Our last action did not involve a question.
+            TwoPlusTwo, // Our last action did not involve a question.
+            OnePlusOne,
+            TwoPlusFive,
+            FivePlusSix,
+            TenPlusOne,
+            ElevenPlusTwentyTwo,
+            FifteenPlusEighteen,
+            TwentyFiveMinusTwentyOne,
+            FiftyMinusEleven,
+            FiftyFourMinusFortyNine,
+            HundredandFourMinusFifteen,
+            TwentyPlusSixtyTwoPlusTwentyTwo,
+            TenPlusTenPlusTen,
+
+        }
+
+    // The last question asked.
+    public Question LastQuestionAsked { get; set; } = Question.None;
+}
+}
---
+++ PromptUsersForInput_final/bin/UserProfile.cs
@@ -0,0 +1,24 @@
+﻿namespace Microsoft.BotBuilderSamples
+{
+    public class UserProfile
+    {
+        public string Name { get; set; }
+
+        public int? Age { get; set; }
+        public int? TwoPlusTwo { get; set; }
+        public int? OnePlusOne { get; set; }
+        public int? TwoPlusFive { get; set; }
+        public int? FivePlusSix { get; set; }
+        public int? TenPlusOne { get; set; }
+        public int? ElevenPlusTwentyTwo { get; set; }
+        public int? FifteenPlusEighteen { get; set; }
+        public int? TwentyFiveMinusTwentyOne { get; set; }
+        public int? FiftyMinusEleven { get; set; }
+        public int? FiftyFourMinusFortyNine { get; set; }
+        public int? HundredandFourMinusFifteen { get; set; }
+        public int? TwentyPlusSixtyTwoPlusTwentyTwo { get; set; }
+        public int? TenPlusTenPlusTen { get; set; }
+
+        public string Date { get; set; }
+    }
+}
